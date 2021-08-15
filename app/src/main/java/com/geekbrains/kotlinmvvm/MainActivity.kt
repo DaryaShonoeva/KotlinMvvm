e8c5c16f3a93acecf7c98b82c54a20f4cc5370c8
DaryaShonoeva <darya_shon@mail.ru>
2021-08-16 01:14:41 +0300

    Initial commit

---
+++ app/src/main/java/com/geekbrains/kotlinmvvm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.geekbrains.kotlinmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekbrains.kotlinmvvm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
